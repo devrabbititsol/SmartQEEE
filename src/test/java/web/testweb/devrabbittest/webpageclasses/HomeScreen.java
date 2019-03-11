6990620195dd12d7a7295949fc49361ff9683b01
santhosh-annam <santhosh.annam@devrabbit.com>
2019-03-11 12:35:01 +0530

    ggggf

---
+++ src/test/java/web/testweb/devrabbittest/webpageclasses/HomeScreen.java
@@ -0,0 +1,32 @@
+package web.testweb.devrabbittest.webpageclasses;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import com.utilities.BaseClass;import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+
+@SuppressWarnings("unused")
+public class HomeScreen extends BaseClass {
+	
+	public static String primaryInfo  = "{\"user_id\":7,\"is_generate\":false,\"is_execute\":false,\"is_web\":true,\"project_url\":\"http://www.devrabbit.com/\",\"report_upload_url\":\"https://192.168.1.142:8080/SmartQE/UploadReportFile\",\"project_name\":\"TestWeb\",\"project_description\":\"d\",\"project_id\":236,\"module_name\":\"DevrabbitTest\",\"module_description\":\"\",\"sub_module_id\":0,\"module_id\":376,\"testcase_name\":\"TC_DevTest\",\"testcase_id\":440,\"testset_id\":0,\"executed_timestamp\":1804705745,\"browser_type\":\"chrome\"}";
+
+	public static String projectName = "testweb";
+	public WebDriver driver;
+	public ExtentReports reports;
+	public ExtentTest test;
+	public static final int datasetsLength = 1;
+
+	public HomeScreen(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	@FindBy(how = How.XPATH, using = "//LI[@id='menu-item-1476']/A[1]")	
+	private WebElement	Contact_300872A;
+	public void clkAContact_300872() {
+		waitForExpectedElement(driver, Contact_300872A);		
+		String text = Contact_300872A.getText();
+		Contact_300872A.click();
+	}
+
+}
---
+++ src/test/java/web/testweb/devrabbittest/webtestclasses/TC_DevTest.java
@@ -0,0 +1,75 @@
+package web.testweb.devrabbittest.webtestclasses;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.ITestResult;
+import org.testng.annotations.Test;
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+import com.configurations.Constants;
+import com.configurations.ExtentConfigurations;
+import web.testweb.devrabbittest.webpageclasses.HomeScreen;
+import com.utilities.BaseClass;
+import com.utilities.ConfigFilesUtility;
+import com.utilities.Utilities;
+import org.json.JSONObject;
+
+@SuppressWarnings("unused")
+public class TC_DevTest extends BaseClass {
+	ExtentReports reports;
+	ExtentTest test;
+	ITestResult result;
+	private Logger logger;
+	private ConfigFilesUtility configFileObj;
+	public boolean isElementDispalyed = false;	public static final int datasetsLength = 1;
+
+	public TC_DevTest() throws Exception {
+		PropertyConfigurator.configure("log4j.properties");
+		logger = Logger.getLogger(TC_DevTest.class);
+		configFileObj = new ConfigFilesUtility();
+		configFileObj.loadPropertyFile("tc_devtest.properties");
+		reports = ExtentConfigurations.getExtentInstance(reportsPath, projectPath, HomeScreen.projectName);
+		test = reports.startTest("TC_DevTest");
+	}
+	
+	public void setUP() throws Exception {
+		String primaryInfo = HomeScreen.primaryInfo;
+		Constants.PRIMARY_INFO = primaryInfo;
+		driver = launchBrowser(new JSONObject(primaryInfo).optString("browser_type"), configFileObj);
+	}
+
+	public void HomeScreenTest(int i) throws Exception {
+
+	 try{
+		Constants.TOTAL_TC = Constants.TOTAL_TC + 1;
+		Constants.IS_TESTCASE = true; Constants.iS_WEB = true;
+		int datasetScreencount = 1;
+		HomeScreen objHomeScreen = PageFactory.initElements(driver, HomeScreen.class);
+		testLogHeader(test, "Verify HomeScreen page");
+		objHomeScreen.clkAContact_300872();
+		printSuccessLogAndReport(test, logger,  "Text is displayed as : ContactLink");
+
+	   } catch (Exception e) {
+		  isElementDispalyed = false;
+		  printFailureLogAndReport(test, logger,  "Element is not found" + e.getLocalizedMessage());
+		}
+	}
+	
+	@Test
+	public void screensTest() throws Exception {
+		isElementDispalyed = true;
+		for(int datasets = 1; datasets <= HomeScreen.datasetsLength; datasets++) {
+			if(!isElementDispalyed) return;			
+			setUP();
+			if(isElementDispalyed) { HomeScreenTest(datasets);}
+			tearDown();
+		}	}
+
+	
+	public void tearDown() throws Exception {
+		reports.endTest(test);
+		reports.flush();
+		driver.quit();
+	}
+}
