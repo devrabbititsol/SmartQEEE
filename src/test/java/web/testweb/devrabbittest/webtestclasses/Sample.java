d7aa7b913ad8cb78ffd9b18f5dacd698a0a10a3e
santhosh-annam <santhosh.annam@devrabbit.com>
2019-03-14 10:53:23 +0530

    sdcd

---
+++ src/test/java/web/testweb/devrabbittest/webtestclasses/Sample.java
@@ -0,0 +1,80 @@
+package web.testweb.devrabbittest.webtestclasses;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.ITestResult;
+import org.testng.annotations.Test;
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+import com.configurations.Constants;
+import com.configurations.ExtentConfigurations;
+import web.testweb.devrabbittest.webpageclasses.HomeScreen;
+import com.utilities.BaseClass;
+import com.utilities.ConfigFilesUtility;
+import com.utilities.Utilities;
+import org.json.JSONObject;
+
+@SuppressWarnings("unused")
+public class Sample extends BaseClass {
+	ExtentReports reports;
+	ExtentTest test;
+	ITestResult result;
+	private Logger logger;
+	private ConfigFilesUtility configFileObj;
+	public boolean isElementDispalyed = false;	public static final int datasetsLength = 1;
+
+	public Sample() throws Exception {
+		PropertyConfigurator.configure("log4j.properties");
+		logger = Logger.getLogger(Sample.class);
+		configFileObj = new ConfigFilesUtility();
+		configFileObj.loadPropertyFile("sample.properties");
+		reports = ExtentConfigurations.getExtentInstance(reportsPath, projectPath, HomeScreen.projectName);
+		test = reports.startTest("Sample");
+	}
+	
+	public void setUP() throws Exception {
+		String primaryInfo = HomeScreen.primaryInfo;
+		Constants.PRIMARY_INFO = primaryInfo;
+		driver = launchBrowser(new JSONObject(primaryInfo).optString("browser_type"), configFileObj);
+	}
+
+	public void HomeScreenTest(int i) throws Exception {
+
+	 try{
+		Constants.TOTAL_TC = Constants.TOTAL_TC + 1;
+		Constants.IS_TESTCASE = true; Constants.iS_WEB = true;
+		int datasetScreencount = 1;
+		HomeScreen objHomeScreen = PageFactory.initElements(driver, HomeScreen.class);
+		testLogHeader(test, "Verify HomeScreen page");
+String text1 = objHomeScreen.clkAContact_300872();
+		if(text1.equalsIgnoreCase(configFileObj.getProperty("Contact"+ i + datasetScreencount))){
+			printSuccessLogAndReport(test, logger,  "Clicked on : " + configFileObj.getProperty("Contact"+ i + datasetScreencount));
+			printSuccessLogAndReport(test, logger,  "Validated Link Text : " + configFileObj.getProperty("Contact"+ i + datasetScreencount));
+		} else {
+			printFailureLogAndReport(test, logger,  "Link Text is not displayed  : " + configFileObj.getProperty("Contact"+ i + datasetScreencount));
+		}
+
+	   } catch (Exception e) {
+		  isElementDispalyed = false;
+		  printFailureLogAndReport(test, logger,  "Element is not found" + e.getLocalizedMessage());
+		}
+	}
+	
+	@Test
+	public void screensTest() throws Exception {
+		isElementDispalyed = true;
+		for(int datasets = 1; datasets <= HomeScreen.datasetsLength; datasets++) {
+			if(!isElementDispalyed) return;			
+			setUP();
+			if(isElementDispalyed) { HomeScreenTest(datasets);}
+			tearDown();
+		}	}
+
+	
+	public void tearDown() throws Exception {
+		reports.endTest(test);
+		reports.flush();
+		driver.quit();
+	}
+}
